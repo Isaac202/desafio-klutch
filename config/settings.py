7462f6f23ed34a26ae98a5496903be8b7a18ef57
Isaac202 <isaacbyguaracity@gmail.com>
2022-01-07 22:56:11 -0300

    Resolvendo problema

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -98,13 +98,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-""" DATABASES = {
+ """
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
- """
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
